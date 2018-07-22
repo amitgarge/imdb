f3c499df3fcf3d0b57b46f61ecde5f53c2475c40
amitgarge <38751675+amitgarge@users.noreply.github.com>
2018-07-22 15:52:55 +0530

    Final Commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+/* This file is responsible for all basic operations like enable or disable the sections*/
 $(document).ready(() => {
 	
+  /*On first load, show 'main' section with focus on 'title' textbox and hide all other section*/ 
   $("#main").show();
   $(".loader").hide();
   $(".time-out").hide();
@@ -7,40 +9,55 @@ $(document).ready(() => {
   $("#Error").hide();
   $("#title").focus();
 
+  /*When search-result section will be shown, only first tab is active initially*/
   $("#Plot-Casting-details").show();
   $("#imdb-details-info").hide();
   $("#others-details").hide();
 
+  /*Making a tab 'active' and others 'deactive'- on click event- by using 'active' class*/
   $(".tab").click(function() {
     $(".tab").removeClass("active");
     $(this).addClass("active");
   });
 
+  /*Clicking first tab*/
   $("#Plot-Casting").click(() => {
     $("#Plot-Casting-details").show();
     $("#imdb-details-info").hide();
     $("#others-details").hide();
   });
 
+  /*Clicking second tab*/
   $("#imdb-details").click(() => {
     $("#Plot-Casting-details").hide();
     $("#imdb-details-info").show();
     $("#others-details").hide();
   });
 
+  /*Clicking third tab*/
   $("#others").click(() => {
     $("#Plot-Casting-details").hide();
     $("#imdb-details-info").hide();
     $("#others-details").show();
   });
   
-  // Reload the entire page and skips to Main page to remove previously entered data.
+  /*Reload the entire page and skips to Main page to remove previously entered data,
+  on cliking the title of the navigation bar*/
   $(".main-title").click(() => {
     location.reload();
   });
 
-  $(".main-title-error").click(() => {
-    $("#main").show();
-    $("#Error").hide();
+  //Reset title and year fields
+  $("#TitleAndYearReset").click((e) => {
+    e.preventDefault();
+    $("#title").val('');
+    $("#year").val('');
+  });
+
+  //Reset ID field
+  $("#ResetIDField").click((e) => {
+    e.preventDefault();
+    $("#ID").val('');
+
   });
 });
--- js/index2.js
+++ js/index2.js
@@ -1,31 +1,32 @@
-$(document).ready(() => {
+/* This js file is responsible for all input and output related operations
+and making the ajax request*/
 
+$(document).ready(() => {
+  /*Clicking on search button of the 'By Title' section*/
   $("#searchByTitleAndYear").click((e) => {
     e.preventDefault();
 
+    /*Validations for the fields*/
     if ($('#title').val() == '' && $("#year").val() == '') {
       alert("Atleast Title is necessary to search.");
 
     } else if ($('#title').val() !== '' && $("#year").val() == '') {
+      /*When, only title is provided*/
       getResponse($('#title').val(), 1);
 
     } else if ($('#title').val() == '' && $("#year").val() != '') {
+      /*When, only year is provided*/
       alert("Title is compulsory.");
 
     } else {
-      // Taking the value of input and calling function with value as parameter.
+      // when both title and year are provided...
       let title = $("#title").val();
       let year = $("#year").val();
       getResponse(title, year);
     }
   });
 
-  $("#TitleAndYearReset").click((e) => {
-    e.preventDefault();
-    $("#title").val('');
-    $("#year").val('');
-  });
-
+  /*Clicking on search button of the 'By ID' section*/
   $("#searchByID").click((e) => {
 
     e.preventDefault();
@@ -34,21 +35,15 @@ $(document).ready(() => {
       alert("ID is compulsory.");
     else
       getResponse($("#ID").val(), 2);
-
-  });
-
-  $("#ResetIDField").click((e) => {
-
-    e.preventDefault();
-    $("#ID").val('');
-
   });
 });
 
+/* Updating the link, calling the "OMDb API", using following function */
 let getResponse = (field, num) => {
 
   let link = "";
 
+  /*Updating the link*/
   // if input is title and num is year then link will be updated with the title and year.
   if ((field !== undefined && num !== 1) && (field !== undefined && num !== 2)) {
     link += `https://www.omdbapi.com/?t=${field}&y=${num}&apikey=55a2e24`;
@@ -60,6 +55,7 @@ let getResponse = (field, num) => {
     link += `https://www.omdbapi.com/?i=${field}&apikey=55a2e24`;
   }
 
+  /* Ajax request*/
   $.ajax({
     type: 'GET',
     dataType: 'json',
@@ -68,31 +64,46 @@ let getResponse = (field, num) => {
 
     success: (response) => {
       if (response.Response === "True") {
+        /*if API Response is true...*/
 
+        /* Displaying search-result section and hiding all other sections*/
         $("#Error").hide();
         $("#search_result").show(2000);
         $("#main").hide();
         $(".totalSeasons").hide();
 
+
+        //Setting the values of the HTML elements...
+
+        //title
         $("#Result_title").text(response.Title);
+
+        //year
         $("#Result_year").text(`(${response.Year})`);
+
+        //Type (movie, series, episode etc.)
         $("#Result_type").text(`(${response.Type})`);
 
+        //Censor Rating
         if (response.Rated === "NOT RATED")
           $("#Result_rated").text("N/A");
         else
           $("#Result_rated").text(response.Rated);
 
-        if (hasNumber(response.Runtime))
+        //Runtime
+        if (hasNumber(response.Runtime)) //cheking is Runtime has a number...
+          //converting runtime (which is mins into hh:mm format)...
           $("#Result_runtime").text(minutesToHours(response.Runtime) + " min(s)");
         else
           $("#Result_runtime").text(response.Runtime);
 
+        //Release Date
         if (response.hasOwnProperty("Released"))
           $("#Result_release-date").text(` ${response.Released}`);
         else
           $("#Result_release-date").text("N/A");
 
+        //Poster
         if (response.hasOwnProperty("Poster")) {
           $("#Result_poster").attr("src", response.Poster);
           $("#Result_poster-small").attr("src", response.Poster);
@@ -101,81 +112,94 @@ let getResponse = (field, num) => {
           $("#Result_poster-small").attr("src", "img/dummy.jpg");
         }
 
+        //Language
         if (response.hasOwnProperty("Language"))
           $("#Result_language").text(` ${response.Language}`);
         else
           $("#Result_language").text("N/A");
 
+        //Genre
         if (response.hasOwnProperty("Genre"))
           $("#Result_genre").text(` ${response.Genre}`);
         else
           $("#Result_genre").text("N/A");
 
+        //Plot
         if (response.hasOwnProperty("Plot"))
           $("#Result_plot").text(response.Plot);
         else
           $("#Result_plot").text("N/A");
 
+        //Actors
         if (response.hasOwnProperty("Actors"))
           $("#Result_actors").text(response.Actors);
         else
           $("#Result_actors").text("N/A");
 
+        //Director
         if (response.hasOwnProperty("Director"))
           $("#Result_director").text(response.Director);
         else
           $("#Result_director").text("N/A");
 
+        //Writer
         if (response.hasOwnProperty("Writer"))
           $("#Result_writer").text(response.Writer);
         else
           $("#Result_writer").text("N/A");
 
-        if (response.hasOwnProperty("imdbID"))
-          $("#imdb-id").text(response.imdbID);
-        else
-          $("#imdb-id").text("N/A");
-
-        if (response.hasOwnProperty("imdbID"))
+        //IMDb ID
+        $("#imdb-id").text(response.imdbID);
+        
+        //IMDb Ratings
+        if (response.hasOwnProperty("imdbRating"))
           $("#imdb-ratings").text(response.imdbRating);
         else
           $("#imdb-ratings").text("N/A");
 
+        //IMDb Votes
         if (response.hasOwnProperty("imdbVotes"))
           $("#imdb-votes").text(response.imdbVotes);
         else
           $("#imdb-votes").text("N/A");
 
+        //Metascore
         if (response.hasOwnProperty("Metascore"))
           $("#metascore").text(response.Metascore);
         else
           $("#metascore").text("N/A");
 
+        //Country
         if (response.hasOwnProperty("Country"))
           $("#country").text(response.Country);
         else
           $("#country").text("N/A");
 
+        //Awards
         if (response.hasOwnProperty("Awards"))
           $("#awards").text(response.Awards);
         else
           $("#awards").text("N/A");
 
+        //DVD
         if (response.hasOwnProperty("DVD"))
           $("#dvd").text(response.DVD);
         else
           $("#dvd").text("N/A");
 
+        //BoxOffice Collection
         if (response.hasOwnProperty("BoxOffice"))
           $("#boxoffice").text(response.BoxOffice);
         else
           $("#boxoffice").text("N/A");
 
+        //Production House
         if (response.hasOwnProperty("Production"))
           $("#prouction").text(response.Production);
         else
           $("#prouction").text("N/A");
 
+        //Website
         if (response.hasOwnProperty("Website")) {
           if (response.Website != "N/A")
             $(".website").html(`
@@ -186,6 +210,7 @@ let getResponse = (field, num) => {
           $(".website").text("N/A");
         }
 
+        //Total seasons (it is visible only when, the 'type' is 'series')
         if (response.hasOwnProperty("totalSeasons")) {
           $(".totalSeasons").show();
           $("#totSeasons").text(response.totalSeasons);
@@ -193,6 +218,7 @@ let getResponse = (field, num) => {
           $(".totalSeasons").hide();
         }
 
+        //Raitings table
         if (response.hasOwnProperty("Ratings")) {
           $(".RatingsTable").empty();
           if (response.Ratings.length != 0) {
@@ -220,13 +246,14 @@ let getResponse = (field, num) => {
           $(".RatingsTable").hide();
 
       } else {
+        //When 'Response returns 'false', 'error view' will be displayed
         $("#search_result").hide();
         $("#main").hide();
         $("#Error").show(2000);
       }
     },
 
-    timeout: 5000,
+    timeout: 5000, //timeout is 5 sec.
 
     error: (request, errorType, errorMessage) => {
       if (errorType === "timeout") {
@@ -234,10 +261,12 @@ let getResponse = (field, num) => {
         $(".time-out").show();
 
       } else {
-        console.log("success");
+        $("#main").hide();
+        $("#Error").show(2000);
       }
     },
 
+    //Loader will be displayed while request is sent and wating for the response
     beforeSend: () => {
       $(".loader").show();
       $("#main").hide();
@@ -249,6 +278,7 @@ let getResponse = (field, num) => {
   });
 }
 
+/*Function converting mins data (which is a string e.g. 143mins) into 02:23*/
 let minutesToHours = (mins) => {
   mins = parseInt(mins);
 
@@ -261,6 +291,9 @@ let minutesToHours = (mins) => {
   return ` ${h}:${m}`;
 }
 
+//Checks for string contains number and returns true or false 
 let hasNumber = (myString) => {
-  return /\d/.test(myString);
+  //The test() method tests for a match in a string and if found return true else false
+  
+  return /\d/.test(myString);//check for the digit or number [0-9] in the myString variable
 }
